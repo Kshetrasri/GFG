dd9898ff08a3d1dea7f7efb963f4e0ca74dfe6fc
Kshetrasri <94117786+Kshetrasri@users.noreply.github.com>
2024-07-28 20:48:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Counting Sort/counting-sort.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class CountSort
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+            String arr = "";
+            arr = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.countSort(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to arrange all letters of a string in lexicographical 
+    //order using Counting Sort.
+    public static String countSort(String arr)
+    {
+        String str ="";
+        char[] ch = arr.toCharArray();
+        Arrays.sort(ch);
+        for(int i=0;i<ch.length;i++)
+        {
+            str = str + ch[i];
+        }
+        return str;
+    }
+}
