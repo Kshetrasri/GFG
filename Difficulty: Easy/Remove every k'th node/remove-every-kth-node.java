22e9d732d8e9bbe6e5ddaf54ff2ba5689e4f046c
Kshetrasri <94117786+Kshetrasri@users.noreply.github.com>
2024-10-07 18:36:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove every k'th node/remove-every-kth-node.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    Node next;
+    int data;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Delete_Kth_Node {
+    Node head;
+    Node tail;
+
+    void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = tail.next;
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node copy = head;
+            for (int i = 1; i < s.length; i++) {
+                Node temp = new Node(Integer.parseInt(s[i]));
+                copy.next = temp;
+                copy = copy.next;
+            }
+
+            int k = Integer.parseInt(in.readLine());
+
+            Node ans = new Solution().deleteK(head, k);
+
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Link list Node
+class Node
+{
+    Node next;
+    int data;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+class Solution {
+    /*You are required to complete this method*/
+    Node deleteK(Node head, int k) 
+    {
+        int count=0;
+        
+        Node temp = head;
+        Node prev = null;
+        while(temp!=null)
+        {
+            count++;
+            if(count % k == 0)
+            {
+                prev.next = temp.next;
+                temp = temp.next;
+            }
+            else
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
