2080fb5ed1fed1c49db0165f5641d598f38f11a2
Kshetrasri RATNAPRAKASH Namburi <94117786+Kshetrasri@users.noreply.github.com>
2024-12-17 22:24:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -2,47 +2,42 @@
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
+        while (t-- > 0) {
             int N = Integer.parseInt(read.readLine());
             Solution ob = new Solution();
             System.out.println(ob.factorial(N));
-        
-System.out.println("~");
-}
+
+            System.out.println("~");
+        }
     }
 }
 // } Driver Code Ends
 
 
+
+
 class Solution
 {
-    static int value=0;
-    static void func(int N,int ans)
+    //static int value=0;
+    static int func(int N,int ans)
     {
         if(N==0)
         {
-            return;
+            return ans;
         }
-        else
-        {
+       
             ans = ans*N;
             N--;
-            value = ans;
-            func(N,ans);
-        }
-        
+          return  func(N,ans);
     }
     static long factorial(int N)
     {
        
-        func(N,1);
-        return value;
+        int ans1 = func(N,1);
+        return ans1;
     }
 }
